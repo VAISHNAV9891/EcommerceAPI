50f3c4c29b000278b74e8c421b0dd4c499cdcff3
vaishnav <khannavaishnav4@gmail.com>
2025-10-14 13:30:13 +0530

    Update

--- models/productSchema.js
+++ models/productSchema.js
@@ -7,7 +7,7 @@ import mongoose from 'mongoose'
 const productSchema = new mongoose.Schema({
     name: {type: String, required: true, trim : true,minLength : 1},
     brand : {type: String, required: true},
-    price: {type: Number, required: [true,'Product Price is required'], min:[0,'Price cannot be negative']},
+    price: {type: Number, required: [true,'Product Price is required'], min:[0,'price cannot be negative !!!']},
     description: {type: String, required: [true,'Product Description is required']},
     category: {type: String, required: true},
     stock: {type: Number, required: true, min:[0,'Stock cannot be negative'],default : 0}, 
