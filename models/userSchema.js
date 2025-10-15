a614fdae0c00e7f6511372c59c6bc55c23e308d5
vaishnav <khannavaishnav4@gmail.com>
2025-10-15 12:09:56 +0530

    User Schema created

---
+++ models/userSchema.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username : {type : String, required : true, unique : true},
+    email : {type : String, required : true, unique : true},
+    password : {type : String, required : true},
+    role : {type : String, enum : ['Customer','admin','Seller','DeliveryBoy'], default : 'Customer'}
+},
+{timestamps : true}//Important Thing
+)
+
+const User = mongoose.model('User',userSchema);
+
+
+export default User;
